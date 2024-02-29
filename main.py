cd00ffc39107d2225afc4e03214a1469a192f555
harishP27 <160263857+harishP27@users.noreply.github.com>
2024-02-29 13:03:13 -0800

    newer file change

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class Game:
         self.power_ups = pg.sprite.Group()
         # self.player1 = Player(self, 1, 1)
         # for x in range(10, 20):
-        #     Wall(self, x, 5) 
+        #     Wall(self, x, 5)
         for row, tiles in enumerate(self.map_data):
             print(row)
             for col, tile in enumerate(tiles):
